e01ad429262f8e886c4967aa54ece554a5f3f808
Shubham1-Kumar <kshubham3684@gmail.com>
2024-09-13 00:01:45 +0530

    Sharing Day 8's learnings

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 8/Caesar Cipher 1/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 8/Caesar Cipher 1/main.py
@@ -1,3 +1,6 @@
+from base64 import decode
+from itertools import count
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
@@ -14,4 +17,39 @@ shift = int(input("Type the shift number:\n"))
 
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
+len_alphabet = len(alphabet)
+def encrypt(original_text, shift_amount):
+    encrypted_text = ""
 
+    for letter in original_text:
+        shifted_index = alphabet.index(letter) + shift_amount
+        shifted_index %= len_alphabet
+        encrypted_text += alphabet[shifted_index]
+    print(f"The encrypted text is :{encrypted_text}")
+def decrypt(encrypted_text,shift_amount):
+    decrypted_text = ""
+    for letter in encrypted_text:
+        shifted_index = alphabet.index(letter) - shift_amount
+        shifted_index %= len_alphabet
+        decrypted_text+= alphabet[shifted_index]
+    print(f"The decrypted text is : {decrypted_text}")
+
+# A combined function for encoding and decoding
+def caesar(text_input,shift_input,encode_decode):
+    output_text = ""
+    if encode_decode == 'encode':
+        shift_input *=1
+    if encode_decode == 'decode':
+        shift_input *=-1
+    for letter in text_input:
+        shifted_index = alphabet.index(letter) +shift_input
+        shifted_index %= len_alphabet
+        output_text += alphabet[shifted_index]
+    print(f"The {encode_decode} text is : {output_text}")
+
+caesar(text_input= text,shift_input=shift,encode_decode=direction)
+# if direction == 'encode':
+#     encrypt(text,shift)
+#
+# if direction == 'decode':
+#     decrypt(text,shift)
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 8/Positional vs Keyword Arguments/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,32 @@
 # Functions with input
+#
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
+def calculate_love_score(name1, name2):
+    count1 = 0
+    count2 =0
+    lower_name = name1+name2
+    print(lower_name)
+    for letter in lower_name:
+        if letter == 't':
+            count1+=1
+        elif letter == 'r':
+            count1 += 1
+        elif letter == 'u':
+            count1+=1
+        elif letter == 'e':
+            count1 += 1
+            count2+=1
+        elif letter == 'l':
+            count2 +=1
+        elif letter == 'o':
+            count2 +=1
+        elif letter == 'v':
+            count2 +=1
+    print(f"{count1}{count2}")
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
-
-
-greet_with_name("Jack Bauer")
+calculate_love_score("Kanye West", "Kim Kardashian")
