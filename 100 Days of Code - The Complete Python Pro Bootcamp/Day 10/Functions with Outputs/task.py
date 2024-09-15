d6654ec703af65ad7a07a1f14c7d479e53b5ae5f
Shubham1-Kumar <kshubham3684@gmail.com>
2024-09-15 12:27:21 +0530

    Sharing the Day 10's learnings

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 10/Functions with Outputs/task.py
@@ -0,0 +1,6 @@
+
+def function_with_output():
+    f_name = input("Enter first name").title()
+    l_name = input("Enter last name").title()
+    print(f_name+" "+l_name)
+function_with_output()
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 9/Blind Auction Project/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 9/Blind Auction Project/main.py
@@ -1,6 +1,8 @@
 from art import logo
 print(logo)
+# creating an empty dictionary to store the biding details
 Auction_detail ={}
+# Creating a function to find the Maximum bid and bidder
 def Winner_of_the_Auction():
     max_bid = 0
     mext_bid = 0
@@ -17,6 +19,7 @@ while Response_for_a_bid.lower() == 'yes':
     # Taking input for the name
     name = input("What is your name :- ?")
 
+   # Ensuring the name and bid is correct/valid
     try:
         int(name)
         print("The NAME CAN'T BE AN INTEGER\nPLEASE ENTER A VALID NAME(OTHER THAN ANY KIND OF NUMBER\n)")
@@ -29,6 +32,7 @@ while Response_for_a_bid.lower() == 'yes':
         except ValueError:
             print("Please enter a valid response for BID.\n This BID can not be accepted")
 
+    # Taking further response for any left bid
     Response_for_a_bid = input("Are there any other bidders? Type 'yes' or 'no'\n")
 
 Winner_of_the_Auction()
